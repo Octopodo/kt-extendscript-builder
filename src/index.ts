c9628867ab61dce7e1954626de71b320d84f8c82
Octopodo <orson_wells40@hotmail.com>
2025-04-02 09:19:01 +0200

    Removed create config imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // src/index.ts
 
 export { BuildOptions } from './types';
-export { createViteConfig } from './config/vite.config';
-export { extendscriptConfig } from './config/vite.es.config';
-export { tsconfigES3, tsconfigTestsES3 } from './tsconfigs/templates';
+
+export { tsconfigES3, tsconfigTestsES3 } from './lib/config/tsconfigTemplates';
