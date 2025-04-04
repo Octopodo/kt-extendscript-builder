11acb84859be87365c0f7f919fabaa0b16c188a7
Octopodo <orson_wells40@hotmail.com>
2025-04-04 19:01:49 +0200

    Excluded fixtures tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig(() => {
-  return {
-    test: {
-      globals: true,
-      environment: 'node',
-      exclude: ['**/node_modules/**', '**/dist/**']
-    }
-  };
+    return {
+        test: {
+            globals: true,
+            environment: 'node',
+            exclude: ['**/node_modules/**', '**/dist/**', '**/*.js', '**/basic-project/**']
+        }
+    };
 });
